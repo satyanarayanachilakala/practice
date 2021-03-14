418227a86ac93940a6b76ff1c02254236ef41a54
satyanarayanachilakala <lvs.chilakala3430@gmail.com>
2021-03-14 11:41:23 +0530

    Second Commit

---
+++ DocumentController.java
@@ -0,0 +1,6 @@
+public class DocumentController{
+public static void main(String[] args)
+{
+System.out.println("Hello World");
+}
+}
---
+++ ProductController.java
@@ -0,0 +1,6 @@
+public class ProductController{
+public static void main(String[] args)
+{
+System.out.println("Hello World");
+}
+}
