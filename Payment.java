7a40bb7e6654c526667a64bc1439b34dd9600b7c
satyanarayanachilakala <lvs.chilakala3430@gmail.com>
2021-03-21 12:20:10 +0530

    Payment

---
+++ Payment.java
@@ -0,0 +1,25 @@
+public class Payment{
+public static void main(String[] args)
+{
+
+String cardNumber="1234567895522";
+String cardTyp="Debit";
+String lastFourDIgits="7845";
+String CVV="136";
+String billingAddress="8-104 near railwaystation road piduguralla";
+int cardBrand=1;
+String type="card";
+
+
+
+System.out.println(cardNumber);
+System.out.println(cardTyp);
+System.out.println(lastFourDIgits);
+System.out.println(CVV);
+System.out.println(billingAddress);
+System.out.println(cardBrand);
+System.out.println(type);
+
+
+}
+}
