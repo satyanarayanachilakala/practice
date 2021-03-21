51fefd3ae29ffe496ad3d3935d20f2d19eece2c2
satyanarayanachilakala <lvs.chilakala3430@gmail.com>
2021-03-21 12:16:14 +0530

    Shipping

---
+++ ShippingAddresses.java
@@ -0,0 +1,30 @@
+public class ShippingAddresses{
+public static void main(String[] args)
+{
+String id="order23";
+String firstName="Satyanarayana";
+String lastName="Chilakala";
+String address1="8-104 Nar aarailwat station raoabckj";
+String address2="536-250 Satryedkjh street";
+String city="Guntur";
+String state="Andhra pradesh";
+String country="India";
+String zip="52413";
+String phone="9989880080";
+
+
+System.out.println(id);
+System.out.println(firstName);
+System.out.println(lastName);
+System.out.println(address1);
+System.out.println(address2);
+System.out.println(city);
+System.out.println(state);
+System.out.println(country);
+System.out.println(zip);
+System.out.println(phone);
+
+
+
+}
+}
---
+++ UserController.java
@@ -0,0 +1,20 @@
+public class UserController{
+public static void main(String[] args)
+{
+String id="order23";
+String ExternalCustomerId="13sayua";
+String firstName="Satyanarayana";
+String lastName="Chilakala";
+String email="lvs.chilakala@gmail.com";
+
+
+System.out.println(id);
+System.out.println(ExternalCustomerId);
+System.out.println(firstName);
+System.out.println(lastName);
+System.out.println(email);
+
+
+
+}
+}
